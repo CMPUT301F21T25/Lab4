a57c585830ead25ad7c803b737270fd9f4ef5f28
Hrose3056 <rose3056@hotmail.com>
2021-09-30 13:23:23 -0600

    add myColor to shape

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,6 +3,7 @@ package com.example.lab4;
 public abstract class Shape {
     private int x;
     private int y;
-    
+
+    String myColor = "Blue";
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
