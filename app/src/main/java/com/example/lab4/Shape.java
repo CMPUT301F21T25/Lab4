88c176b1c142cbb2f2f76723fb0afec25d3a5de6
Sai Ajerla <ajerla@ualberta.ca>
2021-10-04 12:40:04 -0600

    Color updated

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,10 +3,6 @@ package com.example.lab4;
 public abstract class Shape {
     private int x;
     private int y;
-    
-<<<<<<< HEAD
+
     String color = "Black";
-=======
-    String color = "Magenta";
->>>>>>> 214f8ed5b5be820c9b9cd13f7f88419291695a47
 }
